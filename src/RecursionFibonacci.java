4bca6413c04f0a7d12283ff949306b9ed867de68
Asher Ahn <142775372+Asherforgotthepassword@users.noreply.github.com>
2026-08-13 13:45:16 -0700

    Add comments

--- src/RecursionFibonacci.java
+++ src/RecursionFibonacci.java
@@ -3,12 +3,13 @@ public class RecursionFibonacci {
         String bossMan = "Turner";
         System.out.printf("%s was here\n", bossMan);
 
+        // print statement calls recursive function
         System.out.println(fibonacci(9));
     }
 
     public static int fibonacci(int n) {
-        if (n < 2) return n;
+        if (n < 2) return n; // base case
 
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2); // recursive case, calls itself twice in one return statement
     }
 }
